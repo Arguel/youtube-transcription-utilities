8c976981c24d89e6939d994b13050b5f03169415
Enzo Argüello <xlsones003@gmail.com>
2022-04-23 11:54:44 -0300

    MVP

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# youtube-transcription-utilities
-Utilities for youtube transcripts
+# Utilities for YouTube transcripts
+
+This script is created from [Concatenate Transcript](https://greasyfork.org/en/scripts/402811-concatenate-transcript)
+created by [Oliwang](https://greasyfork.org/en/users/302389-oliwang) and
+[Youtube Transcript Snatcher](https://greasyfork.org/en/scripts/421864-youtube-transcript-snatcher)
+created by [GSRHackZ](https://greasyfork.org/en/users/737702-gsrhackz), it is
+thanks to them and some stackoverflow answers that it was possible to create,
+so big shoutout to them!!
+
+I joined the different configurations to make a single script with multiple
+features (in process), but due to lack of time I had to do the code quickly and
+that causes some features to not work correctly (in the future I will fix them)
+
+To use this script you only need an administrator/extension that allows you to
+use it automatically without having to type the code over and over again, I
+personally use Tampermonkey (you can use any other, just copy and paste the code)
+, it can be downloaded on its [official website](https://www.tampermonkey.net/)
+or if you use Google Chrome you can [download the extension](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
---
+++ script.js
@@ -0,0 +1,225 @@
+// ==UserScript==
+// @name                    Transcript tool
+// @description             Various tools for working with transcripts
+// @match                   https://www.youtube.com/watch?v=*
+// @match                   http://www.youtube.com/watch?v=*
+// @version                 0.1.0
+// @author                  Oliwang, GSRHackZ, Arguel
+// @grant                   none
+// @license                 MIT
+// ==/UserScript==
+
+// https://stackoverflow.com/a/707580
+// Your CSS as text
+const styles = `
+  .amazon-style {
+    background-image: linear-gradient(#f7f8fa ,#e7e9ec);
+    border-color: #adb1b8 #a2a6ac #8d9096;
+    border-width: 1px;
+    box-shadow: rgba(255,255,255,.6) 0 1px 0 inset;
+    box-sizing: border-box;
+    color: #0f1111;
+    font-family: "Amazon Ember",Arial,sans-serif;
+    font-size: 13px;
+    padding: 0 11px;
+    text-decoration: none;
+    text-overflow: ellipsis;
+  }
+  .button-14 {
+    border-style: solid;
+    text-align: center;
+    outline: 0;
+    border-radius: 3px;
+    width: 170px;
+    display: inline-block;
+    cursor: pointer;
+    height: 29px;
+    overflow: hidden;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    white-space: nowrap;
+  }
+
+  .button-14:active {
+    border-bottom-color: #a2a6ac;
+  }
+
+  .button-14:active:hover {
+    border-bottom-color: #a2a6ac;
+  }
+
+  .button-14:hover {
+    border-color: #a2a6ac #979aa1 #82858a;
+  }
+
+  .button-14:focus {
+    border-color: #e77600;
+    box-shadow: rgba(228, 121, 17, .5) 0 0 3px 2px;
+    outline: 0;
+  }
+  
+  .panel {
+    line-height:25px;
+    overflow: auto;
+    scroll-behaviour:smooth;
+  }
+`;
+document.head.appendChild(document.createElement("style")).innerHTML = styles;
+
+let displayed = false,
+  body = document.body,
+  copied = false;
+
+let backup,
+  isBackup = false;
+
+const extractText = () => {
+  const array = [];
+  const text = document.getElementsByClassName(
+    "segment-text style-scope ytd-transcript-segment-renderer"
+  );
+  for (let i = 0; i < text.length; i++) {
+    array.push(text[i].innerText);
+  }
+  const transcript = array.join(" ").split(".").join(".\n");
+  const panel = document.getElementsByClassName(
+    "style-scope ytd-transcript-renderer"
+  )[1];
+  if (isBackup == false) {
+    backup = panel.innerHTML;
+    isBackup = true;
+  }
+  return { transcript, panel };
+};
+
+function copy() {
+  if (copied == false) {
+    const { transcript, panel } = extractText();
+    navigator.clipboard.writeText(transcript).then(
+      function () {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+    panel.innerHTML = transcript;
+    panel.classList.remove("ytd-transcript-renderer");
+    panel.classList.add("panel", "amazon-style");
+    copied = true;
+  }
+}
+
+setInterval(function () {
+  if (
+    document.getElementsByClassName(
+      "style-scope ytd-transcript-renderer"
+    )[3] !== undefined &&
+    displayed == false
+  ) {
+    const transcriptTitle = document.querySelectorAll("#title-container")[1];
+
+    // Download transcript button
+    const downloadBtn = document.createElement("button");
+    downloadBtn.innerHTML = "Download transcript";
+    downloadBtn.classList.add("button-14", "amazon-style");
+    downloadBtn.setAttribute("role", "button");
+    downloadBtn.addEventListener("click", function () {
+      const titleText = document.querySelector(
+        'h1[class="title style-scope ytd-video-primary-info-renderer"]'
+      ).innerText;
+      const { transcript } = extractText();
+      download(transcript, `Transcript - ${titleText}.txt`, "text/plain");
+    });
+
+    // Revert changes button
+    const revertBtn = document.createElement("button");
+    revertBtn.innerHTML = "Revert changes";
+    revertBtn.classList.add("button-14", "amazon-style");
+    revertBtn.setAttribute("role", "button");
+    revertBtn.onclick = function () {
+      const { panel } = extractText();
+      panel.innerHTML = backup;
+      panel.classList.remove("panel", "amazon-style");
+      panel.classList.add("ytd-transcript-renderer");
+      downloadBtn.style.display = "block";
+      copied = false;
+    };
+    revertBtn.style.display = "none";
+
+    // Show and copy transcript button
+    const showBtn = document.createElement("button");
+    showBtn.innerHTML = "Show and copy transcript";
+    showBtn.classList.add("button-14", "amazon-style");
+    showBtn.setAttribute("role", "button");
+    showBtn.onclick = function () {
+      copy();
+      downloadBtn.style.display = "none";
+      revertBtn.style.display = "block";
+      this.innerHTML = "Copied!";
+      setTimeout(function () {
+        showBtn.innerHTML = "Copy transcript";
+      }, 1000);
+    };
+
+    // Remove timestamps button
+    const removeBtn = document.createElement("button");
+    removeBtn.innerHTML = "Remove timestamps";
+    removeBtn.classList.add("button-14", "amazon-style");
+    removeBtn.setAttribute("role", "button");
+    const timestamps = document.querySelectorAll(
+      'div[class="segment-start-offset style-scope ytd-transcript-segment-renderer"]'
+    );
+    removeBtn.onclick = function () {
+      for (let i = 0; i < timestamps.length; i++) {
+        timestamps[i].style.display = "none";
+      }
+
+      restoreBtn.style.display = "block";
+    };
+
+    // Restore timestamps button
+    const restoreBtn = document.createElement("button");
+    restoreBtn.innerHTML = "Restore timestamps";
+    restoreBtn.classList.add("button-14", "amazon-style");
+    restoreBtn.setAttribute("role", "button");
+    restoreBtn.addEventListener("click", function () {
+      for (let i = 0; i < timestamps.length; i++) {
+        timestamps[i].style.display = "block";
+      }
+      this.style.display = "none";
+    });
+    restoreBtn.style.display = "none";
+
+    transcriptTitle.appendChild(showBtn);
+    transcriptTitle.appendChild(downloadBtn);
+    transcriptTitle.appendChild(revertBtn);
+    transcriptTitle.appendChild(removeBtn);
+    transcriptTitle.appendChild(restoreBtn);
+
+    displayed = true;
+  }
+}, 1000);
+
+// https://stackoverflow.com/a/30832210
+// Function to download data to a file
+function download(data, filename, type) {
+  var file = new Blob([data], { type: type });
+  if (window.navigator.msSaveOrOpenBlob)
+    // IE10+
+    window.navigator.msSaveOrOpenBlob(file, filename);
+  else {
+    // Others
+    var a = document.createElement("a"),
+      url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
+}
